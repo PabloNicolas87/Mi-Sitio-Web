f3d030a9cc8de07dfe2c3b13bec19971273ce758
Pablo Girone <gironepablo@gmail.com>
2023-01-06 17:29:40 -0300

    Ajustes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next"
 import { NavLink } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
 
 const Header = () => {
     const [t, i18n ] = useTranslation("global");
@@ -8,14 +9,34 @@ const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container">
-                <Link to='/' className="navbar-brand" href="#">
-                    <img src={'/images/logo.png'} alt="Imágen Logo" />
-                </Link>
+                <div className="">
+                    <Link to='/' className="navbar-brand m-0">
+                        <img src={'/images/logo.png'} alt="Imágen Logo" />
+                    </Link>
+                    <div className="btn-group">
+                        <button type="button" className="btn btn-secondary btn-idioma dropdown-toggle m-0" data-bs-toggle="dropdown" aria-expanded="false">
+                            {t("header.select-language")}
+                        </button>
+                        <ul className="dropdown-menu dropdown-menu-end lista-idiomas">
+                            <li>
+                                <button className="dropdown-item d-flex justify-content-between align-items-center" onClick={() => i18n.changeLanguage("es")}>{t("header.spanish")}<img src={'/images/arg.png'} alt={t("header.spanish")} /></button>
+                            </li>
+                            <li>
+                                <button className="dropdown-item d-flex justify-content-between align-items-center" onClick={() => i18n.changeLanguage("por")}>{t("header.portuguese")}<img src={'/images/bra.png'} alt={t("header.portuguese")} /></button>
+                            </li>
+                            <li>
+                                <button className="dropdown-item d-flex justify-content-between align-items-center" onClick={() => i18n.changeLanguage("en")}>{t("header.english")}<img src={'/images/eeuu.png'} alt={t("header.english")} /></button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                
+
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                    <i className="fa-solid fa-bars"></i>
+                    <FaBars />
                 </button>
                 <div className="collapse navbar-collapse d-flex justify-content-md-end" id="navbarScroll">
-                    <ul className="navbar-nav my-2 my-lg-0 px-2">
+                    <ul className="navbar-nav my-2 my-lg-0 px-2 mx-auto mx-md-0">
                         <li className="nav-item py-2 py-lg-0">
                             <NavLink to="/" className="nav-link px-2 about">{t("header.about-me")}</NavLink>
                         </li>
@@ -28,23 +49,7 @@ const Header = () => {
                         <li className="nav-item py-2 py-lg-0">
                             <NavLink to="/Contact" className="nav-link px-2 contact">{t("header.contact")}</NavLink>
                         </li>
-                    </ul>
-                    <div className="btn-group px-2">
-                        <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                            {t("header.select-language")}
-                        </button>
-                        <ul className="dropdown-menu dropdown-menu-end">
-                            <li >
-                                <button className="dropdown-item d-flex justify-content-between" onClick={() => i18n.changeLanguage("es")}>ES<img src={'/images/arg.png'} alt="ENG" /></button>
-                            </li>
-                            <li>
-                                <button className="dropdown-item d-flex justify-content-between" onClick={() => i18n.changeLanguage("por")}>POR<img src={'/images/bra.png'} alt="POR" /></button>
-                            </li>
-                            <li>
-                                <button className="dropdown-item d-flex justify-content-between" onClick={() => i18n.changeLanguage("en")}>EN<img src={'/images/eeuu.png'} alt="ESP" /></button>
-                            </li>
-                        </ul>
-                    </div>     
+                    </ul>    
                 </div>
             </div>
         </nav>
--- src/components/Swiper.js
+++ src/components/Swiper.js
@@ -8,8 +8,23 @@ const SwiperJS = () => {
     const [t] = useTranslation("global");
     return (
         <Swiper
+        breakpoints={{
+            // when window width is >= 640px
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+                slidesPerGroup: 2,
+              },
+              992: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+                slidesPerGroup: 3,
+              },
+        }}
         spaceBetween={50}
-        slidesPerView={3}
+        slidesPerView={1}
+        slidesPerGroup= {1}
+        loop = {true}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
         >
@@ -17,21 +32,21 @@ const SwiperJS = () => {
             <h3>{t("myProjects.project1")}</h3>
             <img src={'/images/nubizate.png'} alt="Nubizate" />
             <div className="btn-primary">
-                <a className="p-2" href="https://nubizate.com/" target="_blank">{t("myProjects.view")}</a>
+                <a className="p-2" href="https://nubizate.com/" rel="noreferrer" target="_blank">{t("myProjects.view")}</a>
             </div>    
         </SwiperSlide>
         <SwiperSlide className='d-flex flex-column justify-content-between p-5'>
             <h3>{t("myProjects.project2")}</h3>
             <img src={'/images/calculadora.png'} alt="Calculadora" />
             <div className="btn-primary">
-                <a className="p-2" href="https://js-curso-xi.vercel.app/" target="_blank">{t("myProjects.view")}</a>
+                <a className="p-2" href="https://js-curso-xi.vercel.app/" rel="noreferrer" target="_blank">{t("myProjects.view")}</a>
             </div>
         </SwiperSlide>
         <SwiperSlide className='d-flex flex-column justify-content-between p-5'>
             <h3>{t("myProjects.project3")}</h3>
             <img src={'/images/Ecommerce.png'} alt="Ecommerce" />
             <div className="btn-primary">
-                <a className="p-2" href="https://react-curso-alpha.vercel.app/" target="_blank">{t("myProjects.view")}</a>
+                <a className="p-2" href="https://react-curso-alpha.vercel.app/" rel="noreferrer" target="_blank">{t("myProjects.view")}</a>
             </div>
         </SwiperSlide>
         </Swiper>
