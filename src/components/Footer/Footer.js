6d1a9bfe7e621127e1b9f4dc18debe116aec46da
Pablo Girone <gironepablo@gmail.com>
2024-03-28 07:25:39 -0300

    Modificaciones

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
             <footer className="footer">
                 <div className="container">
                     <footer className="d-flex flex-wrap flex-column flex-md-row justify-content-between align-items-center py-3">
-                        <div className="col-md-4 d-flex align-items-center">
+                        <div className="col-md-4 d-flex align-items-center py-3 py-md-0">
                             <Logo />
                         </div>
                         <span className="col-md-4 mb-3 mb-md-0 text-muted text-center">{t("footer.rights")}</span>
