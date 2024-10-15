5da4a8c0944fced56ce68793313c753df56c057e
Pablo Girone <gironepablo@gmail.com>
2024-10-15 11:44:58 -0300

    Update Logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
 
     return (
         <button onClick={scrollToTop} style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
-            <img src={'/images/logo.png'} alt="Imágen Logo" width="180" height="71"/>
+            <img src={'/images/logo.png'} alt="Imágen Logo" width="180" height="auto"/>
         </button>
     );
 }
